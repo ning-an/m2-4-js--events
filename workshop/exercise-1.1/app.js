358adfd1d655c329a8e001307821b5ec6498f7eb
Ning An <ning.an@outlook.com>
2020-06-18 16:02:46 -0400

    Exercise1.1-solution

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,23 @@
 // Exercise 1.1
 // ------------
 console.log('exercise 1.1');
+
+const body = document.querySelector('body');
+const result = document.querySelector('.result');
+
+const header = document.createElement('h1');
+header.innerText = 'Be a quicker clicker!';
+body.insertBefore(header, result);
+
+let win = true;
+
+setTimeout(() => {
+    win = false;
+}, 1000);
+
+function handler() {
+    result.innerText = win? 'Bravo': 'Sorry';
+    body.removeEventListener('click', handler);
+}
+
+body.addEventListener('click', handler);
