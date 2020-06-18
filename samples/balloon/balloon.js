8ca5ec9ac79721a2eb57c7c0c1e63cb392c6addd
Ning An <ning.an@outlook.com>
2020-06-18 14:50:06 -0400

    Balloon try

--- samples/balloon/balloon.js
+++ samples/balloon/balloon.js
@@ -1,1 +1,28 @@
 const balloon = document.getElementById('balloon');
+let balloonSize = 10;
+
+function handler() {
+    console.log(balloonSize)
+    if (event.key === 'ArrowUp') {
+        balloonSize += 10;
+        balloon.style.fontSize = `${balloonSize}px`;
+    };
+    if (balloonSize >= 300) {
+        balloon.innerText = '💥';
+        document.removeEventListener('keydown', handler);
+        let explode = setInterval(() => {
+            balloonSize += 20;
+            balloon.style.fontSize = `${balloonSize}px`;
+            if (balloonSize >= 1000) {
+                balloon.style.opacity = 0;
+                clearInterval(explode);
+            }
+        }, 10);
+    }
+    if (event.key === 'ArrowDown') {
+        balloonSize -= 10;
+        balloon.style.fontSize = `${balloonSize}px`;
+    };
+}
+
+document.addEventListener('keydown', handler);
