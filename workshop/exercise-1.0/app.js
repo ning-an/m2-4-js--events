296299b97eb7d925708387f06ebdfae4438f97c7
Ning An <ning.an@outlook.com>
2020-06-18 15:18:27 -0400

    Exercise1-solution

--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -1,3 +1,12 @@
 // Exercise 1.0
 // ------------
 console.log('exercise-1');
+
+const body = document. querySelector('body');
+const msg = document.querySelector('.msg');
+
+function handler() {
+    msg.innerText = 'You are amazing!';
+}
+
+body.addEventListener('click', handler);
